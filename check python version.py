23781e55d3ca4efff6698188dcbd06193648ae03
cheiZh嗷 <zhaomingli26@gmail.com>
2025-06-17 21:20:42 +0530

    Create check python version.py

---
+++ check python version.py
@@ -0,0 +1,20 @@
+# in command prompt if you use Windows
+
+# ☾ To check if your system has Python installed, run this command in the command prompt:
+
+python --version
+
+# ☾ If Python is installed, you will get a result with the version number, like this (This will show latest python version in your pc)
+
+Python 3.13.2
+
+# If you find that you do not have Python installed on your computer, then you can download it for
+# free from the following website: https://www.python.org/
+
+# On macOS or Linux, you can check the Python version by running:
+
+python3 --version
+
+# This will output something like:
+
+Python 3.13.2
